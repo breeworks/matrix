2d4d2d3f56cf6ff1b61e7038d7049ffc5e4a76ad
dishafaujdar <dishachoudhary1102@gmail.com>
2025-03-20 22:55:57 +0530

    deploying lil COR

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,24 +6,12 @@ import cookieParser from "cookie-parser";
 const app = express();
 const PORT = 3000;
 
-const allowedOrigins = ["https://daily-matrix.vercel.app"];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-    allowedHeaders: ["Content-Type", "Authorization"], 
+    origin: ["https://daily-matrix.vercel.app"], 
+    credentials: true, 
   })
 );
-
-app.options("*", cors());
 
 app.use(express.json());
 app.use(cookieParser());
