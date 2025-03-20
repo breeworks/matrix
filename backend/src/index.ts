f1dff8ada6b53a63eb5ec49f5e47cb4a9892b75d
dishafaujdar <dishachoudhary1102@gmail.com>
2025-03-20 22:38:33 +0530

    deploying lil CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,8 +18,13 @@ app.use(
       }
     },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
   })
 );
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use(cookieParser());
 
