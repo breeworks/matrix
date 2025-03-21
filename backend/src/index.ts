58aee58e899ea808c3901b6de5fcadf91eaa60bf
dishafaujdar <dishachoudhary1102@gmail.com>
2025-03-22 01:44:52 +0530

    CORS debugging-2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,10 +6,14 @@ import cookieParser from "cookie-parser";
 const app = express();
 const PORT = 3000;
 
+const allowedOrigins = ["https://daily-matrix.vercel.app","https://matrix-71yc7mlqi-dishas-projects-ab780da9.vercel.app","https://matrix-dishas-projects-ab780da9.vercel.app","https://matrix-ecru.vercel.app"];
+
 app.use(
   cors({
-    origin: ["https://daily-matrix.vercel.app","https://matrix-71yc7mlqi-dishas-projects-ab780da9.vercel.app","https://matrix-dishas-projects-ab780da9.vercel.app","https://matrix-ecru.vercel.app"], 
-    credentials: true, 
+    origin: allowedOrigins, 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
